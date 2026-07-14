bf7cd0e5f821e8ad9135df8ee79d46a9529e79b0
maurico-matos <mauricio.matos@davinci.edu.ar>
2026-07-13 21:31:07 -0300

    actualizacion de codigo

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwZfGCEsln6Kmb-AGlG4s9X2RsM6c0jwI",
-  authDomain: "booknet-51952.firebaseapp.com",
-  projectId: "booknet-51952",
-  storageBucket: "booknet-51952.firebasestorage.app",
-  messagingSenderId: "194613288922",
-  appId: "1:194613288922:web:be95c17eb12b3e565eae3"
-};
+    apiKey: "AIzaSyAwZfGCEsln6Kmb-AGlG4a9X2RaM5c8jwI",
+    authDomain: "booknet-51952.firebaseapp.com",
+    projectId: "booknet-51952",
+    storageBucket: "booknet-51952.firebasestorage.app",
+    messagingSenderId: "194613288922",
+    appId: "1:194613288922:web:be95c17eb12b3e565eeae3"
+  };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
